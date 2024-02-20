a98c55d3382c2a3a55830da7847f0bc12e28a5fe
davidzangaladze <david.zangaladze@gmail.com>
2024-02-20 15:55:11 +0400

    Add project files.

---
+++ Controllers/PrintController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace Html2Pdf.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PrintController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        public PrintController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("pdf")]
+        public IActionResult Index([FromBody] string content)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                return BadRequest("content is empty");
+            }
+
+            try
+            {
+                var decodedContent = HttpUtility.UrlDecode(content);
+                var enginePath = _configuration["BrowserPath"];
+                var commandArgument = _configuration["CommandArgument"];
+
+                var result = Converter.GeneratePdfBytes(enginePath, commandArgument, decodedContent);
+
+                return File(result, "application/pdf");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+        }
+    }
+}
---
+++ Converter.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace Html2Pdf
+{
+    public static class Converter
+    {
+        private const string PDF = "pdf";
+        private const string HTML = "html";
+        public static byte[] GeneratePdfBytes(string enginePath, string commandArgument, string html)
+        {
+            var fileTempate = $"{AppDomain.CurrentDomain.BaseDirectory}tmp.";
+            var pdfFile = $"{fileTempate}{PDF}";
+            var htmlFile = $"{fileTempate}{HTML}";
+
+            File.WriteAllText(htmlFile, html);
+            var cmd = string.Format(commandArgument, pdfFile, htmlFile);
+
+            Process.Start(enginePath, cmd);
+
+            while (!File.Exists(pdfFile))
+            {
+                Task.Delay(1000);
+            }
+
+            var resultButes = File.ReadAllBytes(pdfFile);
+            File.Delete(pdfFile);
+            File.Delete(htmlFile);
+
+            return resultButes;
+        }
+    }
+}
